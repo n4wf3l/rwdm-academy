bee5615e5f9ef0fc7940b3cdbf09ad4714e77a88
n4wf3l <ajari.nawfel@hotmail.com>
2025-03-19 16:20:09 +0100

    fixing bug : protected route

--- src/components/ProtectedRoute.tsx
+++ src/components/ProtectedRoute.tsx
@@ -2,9 +2,20 @@ import { Navigate } from "react-router-dom";
 
 const ProtectedRoute = ({ children }: { children: JSX.Element }) => {
   const token = localStorage.getItem("token");
-  const user = token ? JSON.parse(atob(token.split(".")[1])) : null; // Décode le token JWT
 
-  if (!token || !user || user.role !== "superadmin") {
+  let user = null;
+
+  if (token) {
+    try {
+      user = JSON.parse(atob(token.split(".")[1]));
+    } catch (error) {
+      console.error("Invalid token format:", error);
+      localStorage.removeItem("token"); // Optionnel : supprimer le token invalide
+      return <Navigate to="/auth" replace />;
+    }
+  }
+
+  if (!user || user.role !== "superadmin") {
     return <Navigate to="/auth" replace />;
   }
 
